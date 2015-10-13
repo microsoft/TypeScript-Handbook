2e17d5de7593dbbf52490562b30b40473336adc2
Wesley Wigham <t-weswig@microsoft.com>
2015-10-13 15:30:19 -0700

    nonzero exit code on lint failures so ci rejects

--- lint.js
+++ lint.js
@@ -10,3 +10,14 @@ var options = {
  
 var result = markdownlint.sync(options);
 console.log(result.toString());
+
+var exitCode = 0;
+Object.keys(result).forEach(function (file) {
+	var fileResults = result[file];
+	Object.keys(fileResults).forEach(function (rule) {
+		var ruleResults = fileResults[rule];
+		exitCode += ruleResults.length;
+	});
+});
+
+process.exit(exitCode);

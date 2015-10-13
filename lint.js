d18ea5d0fcf1915b16b24b8ab9ac2b831d5699fc
Wesley Wigham <t-weswig@microsoft.com>
2015-10-13 15:23:41 -0700

    ignore markdown in node_modules

--- lint.js
+++ lint.js
@@ -2,7 +2,7 @@ var markdownlint = require("markdownlint");
 var glob = require("glob");
 
 var options = {
-  files: glob.sync("**/*.md"),
+  files: glob.sync("**/*.md", {ignore: "node_modules/**/*"}),
   config: {
 	  MD013: false // Line length
   }

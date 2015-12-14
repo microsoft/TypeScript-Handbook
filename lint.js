45a59cb7f7cf0e78da1bd47f4b61bd773b0de25a
Daniel Rosenwasser <drosen@microsoft.com>
2015-12-14 14:30:19 -0800

    Added lint check for code blocks that don't align with the preceding valid column.

--- lint.js
+++ lint.js
@@ -1,8 +1,10 @@
 var markdownlint = require("markdownlint");
 var glob = require("glob");
+var fs = require("fs");
 
+var inputFiles = glob.sync("**/*.md", { ignore: "node_modules/**/*" });
 var options = {
-  files: glob.sync("**/*.md", { ignore: "node_modules/**/*" }),
+  files: inputFiles,
   config: {
     MD001: false, // Header levels should only increment by one level at a time
     MD002: false, // First header should be a h1 header
@@ -43,7 +45,12 @@ var options = {
     MD041: false, // First line in file should be a top level header
   }
 };
- 
+
+var fencedCodeBlockInUnorderedListRegEx = /^(\s+\*\s+)\S.+\r?\n(\s*)/m;
+var carriageReturn = 0x0D;
+var lineFeed = 0x0A;
+
+
 var result = markdownlint.sync(options);
 console.log(result.toString());
 
@@ -56,4 +63,60 @@ Object.keys(result).forEach(function (file) {
     });
 });
 
+inputFiles.forEach(function(fileName) {
+    var text = fs.readFileSync(fileName, "utf8")
+    exitCode += checkForImproperlyIndentedFencedCodeBlocks(fileName, text);
+})
+
 process.exit(exitCode);
+
+/**
+ * @param {string} fileName
+ * @param {string} text
+ */
+function checkForImproperlyIndentedFencedCodeBlocks(fileName, text) {
+    var lines = text.split(/\r?\n/g);
+    var numErrors = 0;
+
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        var codeBlockMatch = line.match(/^(\s*)```\S+/);
+
+        if (codeBlockMatch) {
+            var startingColumn = codeBlockMatch[1].length;
+            if (startingColumn === 0 || startingColumn === getCorrectStartingColumnForLine(lines, i)) {
+                continue;
+            }
+
+            numErrors++;
+            console.log(fileName, ": ",
+                        i + 1, ": A fenced code block following a list item must be indented to the first non-whitespace character of the list item.")
+        }
+    }
+
+    return numErrors;
+}
+
+/**
+ * @param {string[]} line
+ * @param {number} lineIndex
+ */
+function getCorrectStartingColumnForLine(lines, lineIndex) {
+    for (var i = lineIndex - 1; i >= 0; i--) {
+        var line = lines[i];
+
+        if (line.length === 0) {
+            continue;
+        }
+
+        var m;
+        if (m = line.match(/^\s*([\*\-]|(\d+\.))\s*/)) {
+            return m[0].length;
+        }
+        if (m = line.match(/^(\s*)/)) {
+            return m[0].length;
+        }
+    }
+
+    return 0;
+}

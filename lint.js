f3cab7bf8e4dab601920094e65565b5e244a3da0
Jesse Vermeulen <43927190+JesseVermeulen123@users.noreply.github.com>
2018-10-14 09:17:21 +0200

    Move markdownlint config into it's own file
    Related to https://github.com/Microsoft/TypeScript-Handbook/issues/872
    
    This allows editors to pick up this config too.

--- lint.js
+++ lint.js
@@ -2,37 +2,12 @@ var markdownlint = require("markdownlint");
 var glob = require("glob");
 var fs = require("fs");
 
+var markdownConfig = require("./.markdownlint.json");
+
 var inputFiles = glob.sync("**/*.md", { ignore: "node_modules/**/*" });
 var options = {
   files: inputFiles,
-  config: {
-    "default": false, // Let's disable all rules by default
-    "heading-increment": "atx", // Header style
-    "ul-style": { style: "asterisk" }, // Unordered list style
-    "list-indent": true, // Inconsistent indentation for list items at the same level
-    "ul-start-left": true, // Consider starting bulleted lists at the beginning of the line
-    "ul-indent": { indent: 4 }, // Unordered list indentation
-    "no-trailing-spaces": true, // Trailing spaces
-    "no-hard-tabs": true, // Hard tabs
-    "no-reversed-links": true, // Reversed link syntax
-    "no-multiple-blanks": true, // Multiple consecutive blank lines
-    "no-missing-space-atx": true, // No space after hash on atx style header
-    "no-multiple-space-atx": true, // Multiple spaces after hash on atx style header
-    "blanks-around-headings": true, // Headers should be surrounded by blank lines
-    "heading-start-left": true, // Headers must start at the beginning of the line
-    "no-trailing-punctuation": { punctuation: ".,;:!" }, // Trailing punctuation in header
-    "no-multiple-space-blockquote": true, // Multiple spaces after blockquote symbol
-    "no-blanks-blockquote": true, // Blank line inside blockquote
-    "ol-prefix": { style: "ordered" }, // Ordered list item prefix
-    "list-marker-space": true, // Spaces after list markers
-    "blanks-around-fences": true, // Fenced code blocks should be surrounded by blank lines
-    "blanks-around-lists": true, // Lists should be surrounded by blank lines
-    "no-bare-urls": true, // Bare URL used
-    "hr-style": "---", // Horizontal rule style
-    "no-space-in-emphasis": true, // Spaces inside emphasis markers
-    "no-space-in-links": true, // Spaces inside link text
-    "fenced-code-language": true, // Fenced code blocks should have a language specifieds
-  }
+  config: markdownConfig,
 };
 
 var result = markdownlint.sync(options);
